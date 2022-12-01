5b2e6972449fb9a13c27eb3876332f131a913e50
yanagswk <yanagimassu@gmail.com>
2022-12-01 09:30:18 +0900

    カラム文字数変更

---
+++ api/database/migrations/2022_12_01_092726_modify_column_to_games_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('item_caption', 5000)->nullable()->comment('ゲーム説明')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('item_caption', 3000)->nullable()->comment('ゲーム説明')->change();
+        });
+    }
+};
